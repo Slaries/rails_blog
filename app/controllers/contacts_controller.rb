fc0ed0d7c199fb9aee9630f6c7c7166bf4527cfb
Slaries <slaries@rambler.ru>
2020-08-11 14:00:05 +0300

    fix error in view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,17 @@
 class ContactsController < ApplicationController
-	def index
-	end
+
+  def new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    @contact.save
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:email, :message)
+  end
+
 end
